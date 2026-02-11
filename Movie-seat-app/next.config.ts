9be3d09959f0ba7f66d1ab06d37a96b270e39641
Felicia Hu <feliciahu1992_@hotmail.com>
2026-02-11 18:11:08 +0100

    Add GitHub Pages deployment workflow

--- Movie-seat-app/next.config.ts
+++ Movie-seat-app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+  basePath: "/movie-seat-booking",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
